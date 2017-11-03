a76f920da27204b3584620d571974fba9e04f50c
Michael Karl <michaelkarl@Michaels-MBP.Speedport_W723_V_Typ_A_1_01_018>
2017-11-04 00:21:23 +0100

    Timer eingebaut

--- App.js
+++ App.js
@@ -2,23 +2,40 @@ import React from 'react';
 import MainView from './MainView';
 
 export default class App extends React.Component {
-  
+
   state = {
     appState: 'stopped',
     remainingTime: '10:10',
     smallBlind: '10',
-    bigBlind: '25'
+    bigBlind: '25',
+    blindLevel: 0,
+    levelTime: 10 * 60 * 1000,
+    timeAbs: null,
+    timeRel: 0,
+    intervalID: null
   }
 
   onPressStart = () => {
-    this.setState({appState: 'started'})
-    this.setState({remainingTime: '55:55'})
-  }  
+    this.setState({ appState: 'started' })
+    this.setState({ timeAbs: new Date() })
+
+
+    this.setState({
+      intervalID:
+      setInterval(() => {
+        let delta = new Date (this.state.levelTime - (this.state.timeRel + (new Date() - this.state.timeAbs)));
+        this.setState({ remainingTime: delta.getMinutes() + ':' + delta.getSeconds() })
+      }, 1000)
+    })
+  }
 
   onPressPause = () => {
-    this.setState({appState: 'stopped'})
-    this.setState({remainingTime: '10:00'})
-  }  
+    this.setState({ appState: 'stopped' })
+    
+    clearInterval(this.state.intervalID);
+    
+    this.setState({ remainingTime: '10:01' })
+  }
 
   render() {
     return (
